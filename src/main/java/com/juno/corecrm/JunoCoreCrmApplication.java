b6e1486377431e69f6126d6650c5443cc76d5d88
juniormaximiano <juniormaximianoadeilson@hotmail.com>
2026-06-18 13:18:45 -0300

    chore: initialize juno-core-crm api

---
+++ src/main/java/com/juno/corecrm/JunoCoreCrmApplication.java
@@ -0,0 +1,13 @@
+package com.juno.corecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunoCoreCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunoCoreCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juno/corecrm/JunoCoreCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.corecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunoCoreCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
